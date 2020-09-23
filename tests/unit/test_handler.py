2e7b48dccb3c2286e383f3a9bfea625b09405581
Jay Viloria <jviloria96744@gmail.com>
2020-09-23 11:53:45 -0700

    Complete unit tests for handler

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,72 +1,60 @@
+import os
 import json
 import pytest
-#from python_etl import app
+from moto import mock_s3
+import boto3
+
+REGION = "us-west-2"
+BUCKET_NAME = "TEST_BUCKET_NAME"
+PREV_DATA = "acg-covid-data.csv"
+CHANGE_LOG = "CHANGE_LOG.csv"
+PROD_NYT_URL = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv"
+PROD_JH_URL = "https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv"
 
 
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
+@pytest.fixture
+def mock_environment_variables(monkeypatch):
+    monkeypatch.setenv("PROD_NYT_URL", PROD_NYT_URL)
+    monkeypatch.setenv("PROD_JH_URL", PROD_JH_URL)
+    monkeypatch.setenv("BUCKET_NAME", BUCKET_NAME)
+    monkeypatch.setenv("PREV_DATA", PREV_DATA)
+    monkeypatch.setenv("CHANGE_LOG", CHANGE_LOG)
 
-    return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
+
+@mock_s3
+def test_lambda_handler(mock_environment_variables):
+    s3 = boto3.client('s3')
+        
+    s3.create_bucket(
+        Bucket=BUCKET_NAME,
+        CreateBucketConfiguration={
+            'LocationConstraint': REGION,
         },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
+    )
+
+    from python_etl import app
+
+    res = app.lambda_handler({"environment": "production"}, "")
+
+    assert res["Status"] == "New Data Loaded"
+
+
+@mock_s3
+def test_lambda_handler_with_prev_data(mock_environment_variables):
+    s3 = boto3.client('s3')
+        
+    s3.create_bucket(
+        Bucket=BUCKET_NAME,
+        CreateBucketConfiguration={
+            'LocationConstraint': REGION,
         },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
-    }
+    )
 
+    with open(os.path.join(os.path.dirname(__file__), 'mock_prev_data.csv'), 'rb') as data:
+        s3.upload_fileobj(data, BUCKET_NAME, 'production/' + PREV_DATA)
 
-def test_lambda_handler():
-    assert 1 == 1
+    from python_etl import app
 
-    # ret = app.lambda_handler(apigw_event, "")
-    # data = json.loads(ret["body"])
+    res = app.lambda_handler({"environment": "production"}, "")
 
-    # assert ret["statusCode"] == 200
-    # assert "message" in ret["body"]
-    # assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
+    assert res["Status"] == "Daily Data Updated"
