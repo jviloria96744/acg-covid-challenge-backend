c5472b13038577c5b7f62bb35cc81862bc57a59f
Jay Viloria <jviloria96744@gmail.com>
2020-09-27 13:46:27 -0700

    Move lambda directories into single directory, modify test_setup.py to account for the new directory structure, modify template.yaml to point to new source code for lambda function resources

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,6 +1,8 @@
 import sys
 import os
 
-root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+root_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '', 'lambdas')
 
 sys.path.append(os.path.join(root_dir, '', 'python_etl'))
+sys.path.append(os.path.join(root_dir, '', 'covid_api'))
+sys.path.append(os.path.join(root_dir, '', 'sns_lambda'))
